9cfe7e172938ae5c2fe2949bad1bf9ad6d132578
Gabriel Ribeiro <gabrielcarleial@gmail.com>
2025-11-18 21:19:17 -0300

    feature(card-historyCard): create AppointmentCardProps interface

---
+++ client/src/components/ui/card/historyCard.tsx
@@ -0,0 +1,8 @@
+interface AppointmentCardProps {
+    appointmentDate: string;
+    appointmentTime: string;
+    doctorName: string;
+    appointmentType: string;
+}
+
+export default function AppointmentCard
