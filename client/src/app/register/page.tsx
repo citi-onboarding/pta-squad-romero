091f4474ff4d2cd90bb6d0277e6a356eb9b6859e
maxine121 <italo.borges1211@gmail.com>
2025-11-24 14:32:43 -0300

    register page structure

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -12,36 +12,70 @@ export default function Register() {
         console.log(data);
     };
 
-
     return(
         <>
             <Header />
-            <div>
-                <h1>Cadastro</h1>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div> 
-                        <input type="" />
-                        <input type="" />
+            <h1 className="font-bold text-5xl">Cadastro</h1>
+            <div className="flex flex-col items-center">
+                <form  onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
+                    <div className="flex gap-6 w-full"> 
+                        <div className="flex flex-col gap-3 w-full">
+                            <p>Nome do Paciente</p>
+                            <input className="p-4 rounded-[8px] border" type="text" placeholder=" Digite aqui..."/>
+                        </div>
+                        <div className="flex flex-col gap-3 w-full">
+                            <p>Nome do Tutor</p>
+                            <input className="p-4 rounded-[8px] border" type="text" placeholder="Digite aqui..."/>
+                        </div>
                     </div>
-                    <div> {/* selection of species */}
+                    <div  className="flex flex-col gap-3 w-full"> {/* selection of species */}
+                        <p>Qual é a espécie do paciente?</p>
+                        <div className="flex gap-[60px] w-full">
+                            <input type="radio" name="especie" value="Ovelha"/>
+                            <input type="radio" name="especie" value="Gato" />
+                            <input type="radio" name="especie" value="Porco" />
+                            <input type="radio" name="especie" value="Vaca" />
+                            <input type="radio" name="especie" value="Cavalo" />
+                            <input type="radio" name="especie" value="Cachorro" />
+                        </div>
+                    </div>
+                    <div className="flex gap-7">
+                        <div className="flex flex-col gap-3">
+                            <p>Idade do Paciente</p>
+                            <input className="p-4 rounded-[8px] border" type="text" placeholder="Digite aqui..."/>
+                        </div>
+                        <div className="flex flex-col gap-3">
+                            <p>Tipo de consulta</p>
+                            <select required className="p-4 rounded-[8px] border">
+                                <option value="">-- Selecione aqui --</option>
+                                <option value="Checkup">Check-up</option>
+                                <option value="Retorno">Retorno</option>
+                                <option value="PrimeiraConsulta">Primeira Consulta</option>
+                                <option value="Vacinacao">Vacinação</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="flex gap-7">
+                        <div className="flex flex-col gap-3">
+                            <p>Médico Responsável</p>
+                            <input className="p-4 rounded-[8px] border" type="text" placeholder="Digite aqui..."/>
+                        </div>
+                        <div className="flex flex-col gap-3">
+                            <p>Data do atendimento</p>
+                            <input className="p-4 rounded-[8px] border" type="date"/>
+                        </div>
+                        <div className="flex flex-col gap-3">
+                            <p>Data do atendimento</p>
+                            <input className="p-4 rounded-[8px] border" type="time" />
+                        </div>
+                    </div>
 
+                    <div className="w-full flex flex-col gap-3">
+                        <p>Descrição do Problema</p>
+                        <textarea className="p-4 rounded-[8px] border w-full" />
                     </div>
-                    <div>
-                        <input />
-                        <input />
-                    </div>
-
-                    <div>
-                        <input />
-                        <input />
-                        <input />
-                    </div>
-
-                    <div>
-                        <textarea />
-                    </div>
-
-                    <RegisterModal />
+                        <RegisterModal />
+                    
                 </form>
             </div>
         </>
