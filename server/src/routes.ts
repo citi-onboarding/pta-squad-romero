2025bd820bca5c5870abc0a4555586ec5c1b7f87
Gustavo José Carneiro Leão Neto <gustavoclneto@192.168.0.11>
2025-11-24 10:28:05 -0300

    feat(routes): add appointment routes for CRUD operations

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "./controllers/UserController";
+import appointmentController from "./controllers/AppointmentController";
 
 const routes = express.Router();
 
@@ -7,5 +8,11 @@ routes.post("/user", userController.create);
 routes.get("/user", userController.get);
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
+
+routes.post("/appointments", appointmentController.create);
+routes.get("/appointments", appointmentController.get);
+routes.get("/appointments/:id", appointmentController.show);
+routes.delete("/appointments/:id", appointmentController.delete);
+routes.put("/appointments/:id", appointmentController.update);
 
 export default routes;
