06b412e9177135ef3562582f3fbba55db5417eb3
Gabriel Ribeiro <gabrielcarleial@gmail.com>
2025-11-25 10:56:38 -0300

    feat(crud-pet): implement CRUD operations for Pet model and update routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "./controllers/UserController";
+import petController from "./controllers/PetController";
 
 const routes = express.Router();
 
@@ -7,5 +8,12 @@ routes.post("/user", userController.create);
 routes.get("/user", userController.get);
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
+
+// Pet Controller
+routes.post("/pet", petController.create);
+routes.get("/pet", petController.get);
+routes.get("/pet/:id", petController.findPetById);
+routes.delete("/pet/:id", petController.delete);
+routes.patch("/pet/:id", petController.update);
 
 export default routes;
