8b70d693ecf9c959e6b30ad77672b931d1bd4d12
romero003 <romerofilho03@gmail.com>
2025-11-28 20:26:44 -0300

    feat(appointment-page-integration): integrate CORS middleware and update dependencies

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import routes from "./routes";
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import "@database";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(express.static(__dirname + "/public"));
