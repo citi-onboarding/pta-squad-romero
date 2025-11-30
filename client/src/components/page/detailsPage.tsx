5547cbf55abdf854fcd66680eae867bb32a90607
Gabriel Ribeiro <gabrielcarleial@gmail.com>
2025-11-30 20:17:26 -0300

    feat(page-details): enhance appointment display with detailed descriptions and dynamic selection

--- client/src/components/page/detailsPage.tsx
+++ client/src/components/page/detailsPage.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter} from "next/navigation";
 import Header from "../header/header";
 import HistoryCard from "../card/historyCard";
@@ -18,10 +18,7 @@ const mockPatients = [
     id: "1",
     petName: "Rex",
     ownerName: "Carlos Silva",
-    petAge: "5 anos",
-    appointmentType: "Consulta de rotina",
-    doctorName : "Dr. João Paulo",
-    problemDescription: "O animal apresenta falta de apetite e cansaço excessivo há 2 dias, O animal apresenta falta de apetite e cansaço excessivo há 2 dias. ",
+    petAge: "5 anos",   
     image: cachorroImg
   },
 
@@ -30,9 +27,6 @@ const mockPatients = [
     petName: "Félix",
     ownerName: "Pedro Costa",
     petAge: "3 anos",
-    appointmentType: "Vacinação",
-    doctorName : "Dra. Camila Andrade",
-    problemDescription: "O animal está com tosse persistente e secreção nasal.",
     image: gatoImg
   },
 ];
@@ -44,7 +38,8 @@ const mockAllAppointments = [
     appointmentDate: "2025-08-12",
     appointmentTime: "14:00",
     doctorName: "Dr. João Paulo",
-    appointmentType: "Consulta de rotina"
+    appointmentType: "Consulta de rotina",
+    problemDescription: "O animal apresenta falta de apetite e cansaço excessivo há 2 dias.",
   },
   {
     id: 102,
@@ -52,7 +47,8 @@ const mockAllAppointments = [
     appointmentDate: "2025-08-13",
     appointmentTime: "09:30",
     doctorName: "Dra. Camila Andrade",
-    appointmentType: "Vacinação"
+    appointmentType: "Vacinação",
+    problemDescription: "O animal está com tosse persistente e secreção nasal."
   },
   {
     id: 103,
@@ -60,7 +56,8 @@ const mockAllAppointments = [
     appointmentDate: "2025-08-14",
     appointmentTime: "11:00",
     doctorName: "Dr. Rafael Moura",
-    appointmentType: "Retorno"
+    appointmentType: "Retorno",
+    problemDescription: "O animal tem febre, coceira na orelha e machucados na pele.",
   },
   {
     id: 104,
@@ -68,7 +65,8 @@ const mockAllAppointments = [
     appointmentDate: "2025-08-15",
     appointmentTime: "16:20",
     doctorName: "Dra. Ana Beatriz",
-    appointmentType: "Exame de sangue"
+    appointmentType: "Exame de sangue",
+    problemDescription: "O animal possui sintomas como enjoo, dificuldade em andar e fraqueza.",
   }
 ];
 // This componente expects a ID prop to identify the pet
@@ -80,13 +78,29 @@ export default function DetailsPage({ petId }: DetailsPageProps) {
     const router = useRouter();
     const currentPatient = mockPatients.find((p) => p.id === petId);
 
+    const petAppointments = mockAllAppointments.filter(app => app.petId === petId);
+    const sortedAppointments = petAppointments.sort((a, b) => {
+        const dateA = new Date(`${a.appointmentDate}T${a.appointmentTime}`);
+        const dateB = new Date(`${b.appointmentDate}T${b.appointmentTime}`);
+        return dateB.getTime() - dateA.getTime(); 
+    });
+    // Controls wich appointment is on screen
+    const [displayedAppointment, setDisplayedAppointment] = useState(sortedAppointments[0] || null);
+
+    // If data changes, state reset
+    useEffect(() => {
+        if (sortedAppointments.length > 0) {
+            setDisplayedAppointment(sortedAppointments[0])
+        }
+    }, [petId]);
     // Filter history based on current patient
     const now = new Date();
     const historyList = mockAllAppointments.filter((appointment) => {
         const isSamePet = appointment.petId === petId;
         const appointmentDateTime = new Date(`${appointment.appointmentDate}T${appointment.appointmentTime}`);
         const isPast = appointmentDateTime < now;
-        return isSamePet && isPast;
+        const isNotDisplayed = appointment.id !== displayedAppointment?.id;
+        return isSamePet && isPast && isNotDisplayed;
     });
 
     // Order history by date descending
@@ -135,19 +149,19 @@ export default function DetailsPage({ petId }: DetailsPageProps) {
                             </div>
                             <div className="justify-end">
                                 <p>{currentPatient.ownerName}</p>
-                                <p>{currentPatient.doctorName}</p>
+                                <p>{displayedAppointment?.doctorName || "-"}</p>
                             </div>
                         </div>
                     </div>
                     {/* Problem Description */}
                     <div className="mt-4">
                         <p className="font-bold text-[15px] mb-2">Descrição do Problema:</p>
-                        <p className="w-full text-[14px] leading-relaxed">{currentPatient.problemDescription}</p>
+                        <p className="w-full text-[14px] leading-relaxed">{displayedAppointment?.problemDescription || "Sem descrição."}</p>
                     </div>
                     {/* Appointment Type */}
                     <div className="mt-4 mr-20 flex flex-row items-center gap-6">
                         <p className="font-bold text-[15px]">Tipo de Consulta:</p>
-                        <p className="text-[15px] bg-blue-200 px-2 py-1 rounded-md">{currentPatient.appointmentType}</p>
+                        <p className="text-[15px] bg-blue-200 px-2 py-1 rounded-md">{displayedAppointment?.appointmentType || "-"}</p>
                     </div>
                     {/* Button to open modal */}
                     <div className="flex flex-col items-center mt-5 w-full border border-gray-300 rounded-2xl p-4 shadow-sm">
@@ -176,6 +190,7 @@ export default function DetailsPage({ petId }: DetailsPageProps) {
                                 appointmentTime={appointment.appointmentTime}
                                 doctorName={appointment.doctorName}
                                 appointmentType={appointment.appointmentType}
+                                onClick={() => setDisplayedAppointment(appointment)}
                             />
                             ))
                         ) : (
