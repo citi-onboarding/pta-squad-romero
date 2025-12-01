bc759c2b67de0b701c6ff5b88c3a2e83d6005d54
romero003 <romerofilho03@gmail.com>
2025-12-01 14:59:54 -0300

    fix(backend-deploy-link): update baseURL for production deployment

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:3001",
+
+    //For local development, replace the current url  by the localhost one:
+    //baseURL: "http://localhost:3001",
+    //Remeber: Dont commit this change for production!
+    baseURL: "https://pta-squad-romero.onrender.com",
 });
 
 
