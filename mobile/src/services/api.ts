59da3b7d8bd83c477d86fa538721758f3f6bdecb
Gabriel Ribeiro <gabrielcarleial@gmail.com>
2025-12-03 10:42:24 -0300

    fix(mobile-integration): update baseURL to use production endpoint and clean up commented code

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.3:3001', 
-//   baseURL: "https://pta-squad-romero.onrender.com",
+  // Put your IP here:
+  //baseURL: 'http://{IP}:3001', 
+  baseURL: "https://pta-squad-romero.onrender.com",
 });
 
 export default api;
