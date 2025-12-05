17ebc8c90dc20322f3ccf12be96d4e7e6a3119e9
romero003 <romerofilho03@gmail.com>
2025-12-05 14:45:33 -0300

    hotFix(cors): testing new cors conf

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,12 @@ import "@database";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'pta-squad-romero.vercel.app',
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }
+));
 app.use(express.json());
 app.use(routes);
 app.use(express.static(__dirname + "/public"));
